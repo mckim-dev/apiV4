7c917e5c40049a2153e37baa000f24cc50aba5a4
student-mckim <mckim01@email.franklin.edu>
2021-11-14 07:44:48 -0500

    add column to U

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_13_145705) do
+ActiveRecord::Schema.define(version: 2021_11_14_104223) do
 
   create_table "questions", force: :cascade do |t|
     t.string "question"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_11_13_145705) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
